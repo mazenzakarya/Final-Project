504bf1251c73c7b70c2a804b275a5b3f7c241362
Mahmoud medhat <mahmoudmodric9@gmail.com>
2025-08-09 10:13:58 -0500

    Add update student Account

--- src/app/Componants/StudentDashBord/navbar/navbar.ts
+++ src/app/Componants/StudentDashBord/navbar/navbar.ts
@@ -1,20 +1,63 @@
+import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { Student } from '../../../service/student';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css'
 })
 export class Navbar {
-    private readonly _Router = inject(Router);
-logout() {
-  // امسح التوكن أو بيانات المستخدم من التخزين
-  localStorage.removeItem('token'); // أو أي اسم خزنته بيه
+  private readonly _Router = inject(Router);
+  loading = false;
+  private readonly _fb = inject(FormBuilder);
+  private studentService = inject(Student);
+  private toastr = inject(ToastrService);
 
-  // إعادة التوجيه لصفحة تسجيل الدخول
-  this._Router.navigate(['/login']);
+
+  showEditForm = false;
+  editForm: FormGroup = this._fb.group({
+    name: ['', Validators.required],
+    passwordHash: ['', Validators.required]
+  });
+
+  openEditForm() {
+    this.showEditForm = true;
+  }
+
+  closeEditForm() {
+    this.showEditForm = false;
+  }
+
+onSubmit() {
+  if (this.editForm.valid) {
+    this.loading = true; // تشغيل التحميل
+
+    this.studentService.UpdateStudent(this.editForm.value).subscribe({
+      next: () => {
+        this.toastr.success('Account updated successfully');
+        this.loading = false; // إيقاف التحميل
+        this.closeEditForm();
+      },
+      error: (err) => {
+        this.toastr.error('Error updating account');
+        console.error(err);
+        this.loading = false; // إيقاف التحميل حتى لو فيه خطأ
+      }
+    });
+  }
+}
+  logout() {
+    // امسح التوكن أو بيانات المستخدم من التخزين
+    localStorage.removeItem('token'); // أو أي اسم خزنته بيه
+
+    // إعادة التوجيه لصفحة تسجيل الدخول
+    this._Router.navigate(['/login']);
+  }
+
 }
 
-}
--- src/app/service/student.ts
+++ src/app/service/student.ts
@@ -6,37 +6,46 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class Student {
-private readonly _HttpClient=inject(HttpClient)
+  private readonly _HttpClient = inject(HttpClient)
 
 
-AddStudent(data: object): Observable<any> {
-  const token = localStorage.getItem('token');
-
-  const headers = new HttpHeaders({
-    'Authorization': `Bearer ${token}`,
-    'Content-Type': 'application/json' // تأكيد على نوع المحتوى
-  });
-
-  return this._HttpClient.post(`https://localhost:7096/api/Students`, data, { headers });
-}
-getAllStudent():Observable<any>
-{
+  AddStudent(data: object): Observable<any> {
     const token = localStorage.getItem('token');
 
-  const headers = new HttpHeaders({
-    'Authorization': `Bearer ${token}`,
-  });
-return this._HttpClient.get(`https://localhost:7096/api/Students`,{headers})
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${token}`,
+      'Content-Type': 'application/json' // تأكيد على نوع المحتوى
+    });
+
+    return this._HttpClient.post(`https://localhost:7096/api/Students`, data, { headers });
+  }
+  getAllStudent(): Observable<any> {
+    const token = localStorage.getItem('token');
+
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${token}`,
+    });
+    return this._HttpClient.get(`https://localhost:7096/api/Students`, { headers })
 
 
-}
+  }
 
-deleteStudent(id: number): Observable<any> {
-  const token = localStorage.getItem('token');
-  const headers = new HttpHeaders({
-    'Authorization': `Bearer ${token}`,
-  });
-  return this._HttpClient.delete(`https://localhost:7096/api/Students/${id}`, { headers });
-}
+  deleteStudent(id: number): Observable<any> {
+    const token = localStorage.getItem('token');
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${token}`,
+    });
+    return this._HttpClient.delete(`https://localhost:7096/api/Students/${id}`, { headers });
+  }
 
+
+  UpdateStudent(data: object): Observable<any> {
+    const user = JSON.parse(localStorage.getItem("user") || '{}');
+
+    const token = localStorage.getItem('token');
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${token}`,
+    });
+    return this._HttpClient.put(`https://localhost:7096/api/Students/update/${user.userId}`, data, { headers });
+  }
 }
